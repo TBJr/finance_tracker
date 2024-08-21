e4fc8ad94a20689c8f1b59d26085154273f7cf7b
TBJ <44341859+TBJr@users.noreply.github.com>
2024-08-21 06:28:03 -0400

    User Authentication

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'tracker/register.html', {'form': form})
