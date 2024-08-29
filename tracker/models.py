f28ab6ef9aa240aa20f1f92863a31e5072531dcc
TBJ <44341859+TBJr@users.noreply.github.com>
2024-08-29 07:13:18 -0400

    mod

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -26,3 +28,10 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.transaction_type} - {self.amount} on {self.date}"
+
+# Signal to create default categories after migrations
+@receiver(post_migrate)
+def create_default_categories(sender, **kwargs):
+    categories = ['Groceries', 'Rent', 'Utilities', 'Salary']
+    for category in categories:
+        Category.objects.get_or_create(name=category)
